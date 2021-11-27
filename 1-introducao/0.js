ebe3ba53ab2acace7d126a6555222796c631d931
Matheus Lima <matheuscl_165@hotmail.com>
2021-11-27 01:58:22 -0300

    first commit of this course

---
+++ 1-introducao/0.js
@@ -0,0 +1,17 @@
+// "Bem resumido, uma expressão regular é um método formal de se especificar um padrão de texto."
+
+// Exemplo, você tem um texto grande, e dentro desse texto você quer encontrar e-mails, CEPs ou outras informações. Como fazer isso? Procurando um conjunto de caracteres específicos dentro desse texto usando REGEX (expressões regulares)
+
+/*
+* 2. Onde encontrar expressões regulares:
+* 2.1 Linguagens de Programação
+* 2.2 Editores de Texto
+* 2.3 IDEs
+* 2.4 Ferramentas de linha de comando
+*/
+
+/*
+* Linguagens vs REGEX (Sabores)
+* 3.1. Cap 2 vou mostrar como executar em algumas linguagens... Java, Ruby, Python, Go e JS
+* 3.2 O curso será em JS, mas o conhecimento não está restrito a JS.
+*/
---
+++ 2-executando-expressoes-regulares/9-entendo-as-flags.js
@@ -0,0 +1,23 @@
+// Duas flags:
+
+// g -> representa Global. Se estiver procurando (sem a flag 'g') a letra 'a', em um texto, e ele encontrar o primeiro 'a' no texto ele vai parar a busca, já com a flag 'g', ele continua buscando o restante das letras 'a' no texto.
+
+// i -> ignora letras maíusculas e letras minúsculas. Ele vai tanto achar uma letra maíscula ou minúscula quando você fizer uma busca que envolva string.
+
+const texto = "Carlos assinou o abaixo-assinado. 3"
+// A expressão abaixo, significa 'encontre na variável texto a letra 'C' OU ('|') as letras 'ab' 
+console.log(texto.match(/C|ab/))
+console.log(texto.match(/C|ab/)[0]) // No índice [0] do retorno da função .match está a string encontrada pelo regex.
+
+console.log(`.charAt encontrou: ${texto.charAt(texto.match(/C|ab/).index)}`) // No argumento do charAt(param) você passa o índice(number) do caractere que está na string
+
+console.log(texto.charAt(3).toLocaleUpperCase()) // O método string.toLocaleUpperCase() faz o mesmo que string.toUpperCase(). Porém, é mais recomendado usar o string.toLocaleUpperCase() pois ele retornar a string para maíuscula (sem alterar a string original), mas de acordo com as configurações de idioma do navegador, sem ocorrer conflitos de mapeamento unicode, o que acontece usando o string.toUpperCase().
+
+console.log(`indexOf 0: ${texto.indexOf(3)}`) // string.indexOf(param), no parâmetro do string.índexOf(param) você passa o argumento do tipo string, caso você passe um número, ele tentará encontrar esse número (caractere) dento da string que ele foi aplicado.
+console.log(`indexOf 1: ${texto.indexOf('-')}`)
+console.log(`indexOf 2: ${texto.indexOf(texto.match(/C/))}`)
+console.log(`indexOf 3: ${texto.indexOf(texto.match(/C/)[0])}`)
+
+
+// Método(.keys(param)) muito louco da função nativa 'Object':
+texto.match(/C/)[Object.keys(texto.match(/C/))[1]]
